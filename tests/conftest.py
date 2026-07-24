f073e1853bc7832c76a468ea6cf05570943d0061
Alexandre Foley <alexandre.foley@usherbrooke.ca>
2026-07-24 09:59:03 -0400

    i give up on this podman in podman quadlet test.

--- tests/conftest.py
+++ tests/conftest.py
@@ -56,24 +56,23 @@ def running_container(podman_client:PodmanClient, built_image):
       --systemd=true starr-test:latest
     """
     name = f"starr-test-{uuid.uuid4()}"
-    podman_client.containers.run(
-        built_image,
-        detach=True,
-        name=name,
-        ports={"7878/tcp": 7878, "8989/tcp": 8989, "9696/tcp": 9696},
-        environment={
-            "RADARR__AUTH__APIKEY": API_KEY,
-            "RADARR__SERVER__PORT": "7878",
-            "SONARR__AUTH__APIKEY": API_KEY,
-            "SONARR__SERVER__PORT": "8989",
-            "PROWLARR__AUTH__APIKEY": API_KEY,
-            "PROWLARR__SERVER__PORT": "9696",
-        },
-        systemd="true",
-    )
-    yield name
     try:
+        podman_client.containers.run(
+            built_image,
+            detach=True,
+            name=name,
+            ports={"7878/tcp": 7878, "8989/tcp": 8989, "9696/tcp": 9696},
+            environment={
+                "RADARR__AUTH__APIKEY": API_KEY,
+                "RADARR__SERVER__PORT": "7878",
+                "SONARR__AUTH__APIKEY": API_KEY,
+                "SONARR__SERVER__PORT": "8989",
+                "PROWLARR__AUTH__APIKEY": API_KEY,
+                "PROWLARR__SERVER__PORT": "9696",
+            },
+            systemd="true",
+        )
+        yield name
+    finally:
         podman_client.containers.get(name).stop()
         podman_client.containers.get(name).remove()
-    except Exception:
-        pass
